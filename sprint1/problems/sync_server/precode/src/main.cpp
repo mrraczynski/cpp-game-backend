c37760038213912a4301eff9d12fddb08a24f9dc
mrraczynski <55063339+mrraczynski@users.noreply.github.com>
2022-10-18 20:17:00 +0300

    18.10.2022

--- sprint1/problems/sync_server/precode/src/main.cpp
+++ sprint1/problems/sync_server/precode/src/main.cpp
@@ -175,6 +175,7 @@ int main() {
     constexpr unsigned short port = 8080;
 
     tcp::acceptor acceptor(ioc, { address, port });
+    std::cout << std::unitbuf;
     std::cout << "Server has started..."sv << std::endl;
     while (true) {
         tcp::socket socket(ioc);
