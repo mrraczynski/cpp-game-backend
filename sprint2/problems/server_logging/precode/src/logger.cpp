f89581a4cfaa53c0a556368070386bb900fac649
mrraczynski <55063339+mrraczynski@users.noreply.github.com>
2022-11-06 19:40:54 +0300

    06.11.2022

--- sprint2/problems/server_logging/precode/src/logger.cpp
+++ sprint2/problems/server_logging/precode/src/logger.cpp
@@ -1,5 +1,15 @@
 #include "logger.h"
 
+#include <boost/json.hpp>
+#include <boost/log/core.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/date_time.hpp>
+
+BOOST_LOG_ATTRIBUTE_KEYWORD(timestamp, "TimeStamp", boost::posix_time::ptime)
+BOOST_LOG_ATTRIBUTE_KEYWORD(additional_data, "AdditionalData", boost::json::value)
+
 void MyFormatter(boost::log::record_view const& rec, boost::log::formatting_ostream& strm)
 {
     // чтобы поставить логеры в равные условия, уберём всё лишнее
