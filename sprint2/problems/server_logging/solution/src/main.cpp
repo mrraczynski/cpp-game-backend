d9926398d8ede548bd21f875e396c5563c0513f9
mrraczynski <55063339+mrraczynski@users.noreply.github.com>
2022-11-08 04:37:38 +0300

    08.11.2022 4

--- sprint2/problems/server_logging/solution/src/main.cpp
+++ sprint2/problems/server_logging/solution/src/main.cpp
@@ -72,7 +72,7 @@ int main(int argc, const char* argv[]) {
         
 
         // Эта надпись сообщает тестам о том, что сервер запущен и готов обрабатывать запросы
-        //std::cout << "Server has started..."sv << std::endl;
+        std::cout << "Server has started..."sv << std::endl;
         BOOST_LOG_TRIVIAL(info) << boost::log::add_value(logger::additional_data,
             logger::LoggerData::GetDataJson("port", std::to_string(port),
                 "address", address.to_string()))
