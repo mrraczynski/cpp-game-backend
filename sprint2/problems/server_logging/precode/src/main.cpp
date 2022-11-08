a882e58c01112be473240db0a3ff0f2829746555
mrraczynski <55063339+mrraczynski@users.noreply.github.com>
2022-11-08 04:57:16 +0300

    08.11.2022 5

--- sprint2/problems/server_logging/precode/src/main.cpp
+++ sprint2/problems/server_logging/precode/src/main.cpp
@@ -72,11 +72,11 @@ int main(int argc, const char* argv[]) {
         
 
         // Эта надпись сообщает тестам о том, что сервер запущен и готов обрабатывать запросы
-        std::cout << "Server has started..."sv << std::endl;
+        //std::cout << "Server has started..."sv << std::endl;
         BOOST_LOG_TRIVIAL(info) << boost::log::add_value(logger::additional_data,
             logger::LoggerData::GetDataJson("port", std::to_string(port),
                 "address", address.to_string()))
-            << "server started"sv;
+            << "Server has started..."sv;
 
         // 6. Запускаем обработку асинхронных операций
         RunWorkers(std::max(1u, num_threads), [&ioc] {
