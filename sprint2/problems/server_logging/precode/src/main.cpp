5c6bc34aa97db1ffce6af067aa64ebec0bbc2bb2
mrraczynski <55063339+mrraczynski@users.noreply.github.com>
2022-11-14 05:49:24 +0300

    14.11.2022

--- sprint2/problems/server_logging/precode/src/main.cpp
+++ sprint2/problems/server_logging/precode/src/main.cpp
@@ -59,7 +59,7 @@ int main(int argc, const char* argv[]) {
             });
 
         // 4. Создаём обработчик HTTP-запросов и связываем его с моделью игры
-        http_handler::RequestHandler handler{ game, "E:/Projects/GitHub/cpp-game-backend/sprint2/problems/server_logging/precode/static1/" /*argv[2]*/ };
+        http_handler::RequestHandler handler{ game, "E:/Projects/GitHub/cpp-game-backend/sprint2/problems/server_logging/precode/static/" /*argv[2]*/ };
 
         //http_handler::LoggingRequestHandler logging_handler{ std::forward<http_handler::RequestHandler>(handler) };
 
